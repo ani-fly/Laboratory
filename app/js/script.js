66a3aa01f4497e3e21f21c386d4ddae3c4fae3e6
Анна Шимчук <crocus763@gmail.com>
2025-10-24 16:50:42 +0300

    створено id для ім'я та секція contact черз click

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Resume site loaded ✅");
+  function setUserName() {
+    const firstNameElement = document.getElementById('firstName');
+    const lastNameElement = document.getElementById('lastName');
+    
+    if (firstNameElement && lastNameElement) {
+      firstNameElement.textContent = 'JOHN';
+      lastNameElement.textContent = 'PARKER';
+    }
+  }
+
+  const section = document.querySelector('.contact-section');
+  const title = section.querySelector('.section-title');
+
+  title.addEventListener('click', () => {
+    section.classList.toggle('collapsed');
+  });
+
+  setUserName();
 });
